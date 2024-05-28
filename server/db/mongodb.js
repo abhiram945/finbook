dca0b3c32e72f0a3c715077d8749e321544b5d56
abhiram945 <abhiram945@gmail.com>
2024-05-28 18:06:24 +0530

    reseted server

--- server/db/mongodb.js
+++ server/db/mongodb.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const URL = process.env.MONGODB_ATLAS_URL;
 mongoose.connect(process.env.MONGODB_ATLAS_URL).then(() => { console.log("Connected to MONGODB...") }).catch(() => { console.log("Connection to MONGODB failed...") });
 
 const user = new Schema({
