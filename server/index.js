db68dbe67cd4f97a938a94ac3ec7ed4b28ac0ca3
abhiram945 <abhiram945@gmail.com>
2024-05-28 16:45:09 +0530

    added client url in server

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const newUserSchema = require("./db/mongodb.js");
 
 app.use(bodyParser.json());
 app.use(express.json())
-app.use(cors());
+app.use(cors({origin:process.env.CLIENT_URL}));
 
 app.listen(8080, () => {
     console.log("Server running on port 8080");
