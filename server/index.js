e1dd85e1b050237f42d1176533302ad19aa61197
abhiram945 <abhiram945@gmail.com>
2024-05-28 17:02:40 +0530

    changed cors

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const newUserSchema = require("./db/mongodb.js");
 
 app.use(bodyParser.json());
 app.use(express.json())
-app.use(cors({origin:process.env.CLIENT_URL}));
+app.use(cors({origin:"*"}));
 
 app.listen(8080, () => {
     console.log("Server running on port 8080");
