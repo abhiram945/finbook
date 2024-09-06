08a86e95b32df4ffb1e08df8687226f32f22151b
abhiram945 <abhiram945@gmail.com>
2024-09-07 02:02:26 +0530

    fixing cors on vercel

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToekn = async (user, res) => {
   res.cookie("jwt", token, {
     httpsOnly: true,
     secure: true,
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
   return token;
