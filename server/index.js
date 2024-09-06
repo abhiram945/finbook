21973ae9711cf3ea978a5e171e65fc7406bfdd60
abhiram945 <abhiram945@gmail.com>
2024-09-07 01:26:04 +0530

    fixing cors

--- server/index.js
+++ server/index.js
@@ -15,7 +15,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(
+  {origin:"https://finbook.vercel.app"}
+));
 
 app.get("/",(req,res)=>{
   res.send("Server is running...")
