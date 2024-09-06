f343e93695e536f9916aafb31a96e9225185bd53
abhiram945 <abhiram945@gmail.com>
2024-09-07 01:32:42 +0530

    fixing cors error

--- server/index.js
+++ server/index.js
@@ -15,9 +15,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors(
-  {origin:"https://finbook.vercel.app"}
-));
+const corsOptions = {
+  origin: 'https://finbook.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
+app.use(cors(corsOptions));
 
 app.get("/",(req,res)=>{
   res.send("Server is running...")
