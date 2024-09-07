59ddc2e916c9585f8257356a1198a78a7729cf7b
abhiram945 <abhiram945@gmail.com>
2024-09-07 11:22:27 +0530

    fixing cors

--- server/index.js
+++ server/index.js
@@ -16,7 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors());
+app.use(cors({
+  origin:"*"
+}));
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/villages", villageRouter);
