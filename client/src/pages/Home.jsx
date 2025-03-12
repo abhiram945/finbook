ceef27d1c15908ca86cab0411fb88f8a0e065162
abhiram945 <abhiramdev945@gmail.com>
2025-03-12 15:46:21 +0530

    fixing download app

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,19 +6,19 @@ const Home = () => {
   function getAndroidCpuArchitecture() {
     const platform = navigator.platform.toLowerCase();
     if (!platform.includes("linux")) {
-        return "universal";
+      return "universal";
     }
     if (platform.includes("armv8") || platform.includes("aarch64") || platform.includes("arm64")) {
-        return "arm64-v8a";
+      return "arm64-v8a";
     } else if (platform.includes("arm")) {
-        return "armeabi-v7a";
+      return "armeabi-v7a";
     } else if (platform.includes("x86_64")) {
-        return "x86_64";
+      return "x86_64";
     } else if (platform.includes("x86")) {
-        return "x86";
+      return "x86";
     }
     return "universal";
-}
+  }
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -38,7 +38,7 @@ const Home = () => {
       observer.observe(element);
     });
     const apkType = getAndroidCpuArchitecture();
-    setApkUrl(apkType)
+    setApkUrl(`/assets/app/finbook-${apkType}.apk`)
     return () => observer.disconnect();
   }, []);
 
@@ -63,16 +63,16 @@ const Home = () => {
         <p data-delay={150} className="animate mt-4 text-[var(--gray)] max-w-2xl mx-auto">Track, manage and optimize your finances with ease</p>
         <p data-delay={225} className="animate text-2xl font-semibold mt-2 md:mt-4">No internet needed, and no more worries about data loss</p>
         <div data-delay={300} className="animate my-6 flex justify-center gap-4">
-          <a href={`/assets/app/finbook-${apkUrl}.apk`} className="bg-[var(--primary)] px-4 md:px-6 py-3 rounded-lg text-white cursor-pointer">Download App</a>
+          <a href={apkUrl} download={"finbook.apk"} className="bg-[var(--primary)] px-4 md:px-6 py-3 rounded-lg text-white cursor-pointer">Download App</a>
           <a className="border border-[var(--primary)] text-[var(--primary)] px-2 md:px-6 py-3 rounded-lg cursor-pointer hover:bg-[var(--primary)] hover:text-white" href="#web">
             Check previous work
           </a>
         </div>
         <div className="flex flex-wrap justify-center items-center gap-8 px-2 md:px-0">
           {[2, 3, 4].map((number, index) => (
-            <div key={number} data-delay={300+((index+1)*75)}
-            className={`animate md:w-48 lg:w-64 h-max p-1 bg-[var(--primaryLight)] shadow-[0_0_1rem_0.25rem_var(--primaryDark)] rounded-xl overflow-hidden ${number === 3 ? "md:w-52 lg:w-80" : "" }`}>
-              <img className="rounded-xl" src={`/assets/app/finbook${number}.jpg`} alt={`finbook${number}`}/>
+            <div key={number} data-delay={300 + ((index + 1) * 75)}
+              className={`animate md:w-48 lg:w-64 h-max p-1 bg-[var(--primaryLight)] shadow-[0_0_1rem_0.25rem_var(--primaryDark)] rounded-xl overflow-hidden ${number === 3 ? "md:w-52 lg:w-80" : ""}`}>
+              <img className="rounded-xl" src={`/assets/app/finbook${number}.jpg`} alt={`finbook${number}`} />
             </div>
           ))}
         </div>
@@ -85,7 +85,7 @@ const Home = () => {
         <div className="flex flex-wrap justify-center items-center gap-8 px-2 pt-6 md:pt-10 md:px-0">
           {[6, 7, 8].map((number, index) => (
             <div key={number} data-delay={index * 100} className="animate md:w-48 lg:w-64 h-max p-1 bg-[var(--primaryLight)] shadow-[0_0_1rem_0.25rem_var(--primaryDark)] rounded-xl overflow-hidden">
-              <img className="w-full h-full rounded-xl" src={`/assets/app/finbook${number}.jpg`} alt={`finbook${number}`}/>
+              <img className="w-full h-full rounded-xl" src={`/assets/app/finbook${number}.jpg`} alt={`finbook${number}`} />
             </div>
           ))}
         </div>
@@ -105,33 +105,33 @@ const Home = () => {
                   <p className="text-[var(--gray)]">✔ One-Click G-Drive Backup & Restore</p>
                 </div>
               )}
-              <img className="w-full h-full rounded-xl" src={`/assets/app/finbook${number}.jpg`} alt={`finbook${number}`}/>
+              <img className="w-full h-full rounded-xl" src={`/assets/app/finbook${number}.jpg`} alt={`finbook${number}`} />
             </div>
           ))}
         </div>
       </div>
 
-      {[webv2Descriptions,webv1Descriptions].map((descriptions,descriptionsIndex)=>
-      <div className="p-2 md:pb-4 px-4 pt-16" id="web" key={descriptionsIndex}>
-        <h2 className="animate text-4xl md:text-6xl font-bold text-center">
-          <span className="text-[var(--primary)]">Finbook</span> website v{descriptionsIndex===0 ? "2" : "1"}{" "}
-          <span className="font-normal text-sm text-[var(--red)] bg-[var(--redLight)] shadow px-2 py-1 align-middle rounded-3xl tracking-wider">
-            discontinued
-          </span>
-        </h2>
-        {descriptionsIndex===0 && <p className="animate my-4 font-semibold text-xl text-center">
-          A successful step towards transitioning to an enhanced version of the app with more powerful features.
-        </p>}
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 px-2 pt-6 md:pt-10">
-          {descriptions.map((description, index) => (
-            <div key={index} data-delay={index * 100} className="animate bg-[var(--primaryLight)] shadow-[0_0_1rem_0rem_var(--primaryDark)] rounded-lg overflow-hidden p-2">
-              <p className="text-lg mb-2 font-semibold text-gray-200">{description}</p>
-              <img className="rounded-lg" src={`/assets/web/finbookv${descriptionsIndex===0 ? "2" : "1"}-${index + 1}.png`} alt={`finbookv2-${index + 1}.png`}/>
-            </div>
-          ))}
-        </div>
-        {descriptionsIndex===0 && <div className="animate flex justify-center items-center gap-4 text-lg mt-8"><p>Want to give it a try?</p><a href="#app" className="bg-[var(--primary)] px-4 py-2 rounded-3xl">Download App</a></div>}
-      </div>)}
+      {[webv2Descriptions, webv1Descriptions].map((descriptions, descriptionsIndex) =>
+        <div className="p-2 md:pb-4 px-4 pt-16" id="web" key={descriptionsIndex}>
+          <h2 className="animate text-4xl md:text-6xl font-bold text-center">
+            <span className="text-[var(--primary)]">Finbook</span> website v{descriptionsIndex === 0 ? "2" : "1"}{" "}
+            <span className="font-normal text-sm text-[var(--red)] bg-[var(--redLight)] shadow px-2 py-1 align-middle rounded-3xl tracking-wider">
+              discontinued
+            </span>
+          </h2>
+          {descriptionsIndex === 0 && <p className="animate my-4 font-semibold text-xl text-center">
+            A successful step towards transitioning to an enhanced version of the app with more powerful features.
+          </p>}
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 px-2 pt-6 md:pt-10">
+            {descriptions.map((description, index) => (
+              <div key={index} data-delay={index * 100} className="animate bg-[var(--primaryLight)] shadow-[0_0_1rem_0rem_var(--primaryDark)] rounded-lg overflow-hidden p-2">
+                <p className="text-lg mb-2 font-semibold text-gray-200">{description}</p>
+                <img className="rounded-lg" src={`/assets/web/finbookv${descriptionsIndex === 0 ? "2" : "1"}-${index + 1}.png`} alt={`finbookv2-${index + 1}.png`} />
+              </div>
+            ))}
+          </div>
+          {descriptionsIndex === 0 && <div className="animate flex justify-center items-center gap-4 text-lg mt-8"><p>Want to give it a try?</p><a href="#app" className="bg-[var(--primary)] px-4 py-2 rounded-3xl">Download App</a></div>}
+        </div>)}
 
       {/* Footer */}
       <div className="p-4 container mx-auto">
